17cbb53c748b182a0a8697c589d78336eff57c74
laole918 <laole918@qq.com>
2022-04-18 10:35:40 +0800

    fix 自渲染cell复用时重影

--- XWAdSDKDemo/native/cell/XWNativeAdImageCell.m
+++ XWAdSDKDemo/native/cell/XWNativeAdImageCell.m
@@ -7,7 +7,24 @@
 
 #import "XWNativeAdImageCell.h"
 
+@interface XWNativeAdImageCell ()
+
+@property (nonatomic, strong) UIImageView *iconImageView;
+@property (nonatomic, strong) UILabel *title;
+@property (nonatomic, strong) UILabel *desc;
+
+@end
+
 @implementation XWNativeAdImageCell
+
+- (instancetype) init {
+    if (self = [super init]) {
+        self.iconImageView = [[UIImageView alloc] init];
+        self.title = [[UILabel alloc] init];
+        self.desc = [[UILabel alloc] init];
+    }
+    return self;
+}
 
 - (void)setupWithNativeAdDataObject:(XWNativeAdDataObject *)dataObject delegate:(id <XWNativeAdViewDelegate>)delegate vc:(UIViewController *)vc {
     self.adView.delegate = delegate; // adView 广告回调
@@ -15,45 +32,45 @@
     //一定要先refreshData
     [self.adView refreshData:dataObject];
     //在refreshData之后添加view
+    [self.adView addSubview:self.iconImageView];
+    [self.adView addSubview:self.title];
+    [self.adView addSubview:self.desc];
+    
     CGFloat width = CGRectGetWidth([UIScreen mainScreen].bounds) - 20;
     CGFloat bannerHeigh = 52;
     
-    UIImageView *iconImageView = [[UIImageView alloc] init];
     if (dataObject.iconUrl) {
         NSURL *iconURL = [NSURL URLWithString:dataObject.iconUrl];
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
             NSData *iconData = [NSData dataWithContentsOfURL:iconURL];
             dispatch_async(dispatch_get_main_queue(), ^{
-                iconImageView.image = [UIImage imageWithData:iconData];
+                self.iconImageView.image = [UIImage imageWithData:iconData];
             });
         });
     }
-    [self.adView addSubview:iconImageView];
-    iconImageView.frame = CGRectMake(0, 0, bannerHeigh, bannerHeigh);
-    iconImageView.backgroundColor = [UIColor clearColor];
+    self.iconImageView.frame = CGRectMake(0, 0, bannerHeigh, bannerHeigh);
+    self.iconImageView.backgroundColor = [UIColor clearColor];
     CGFloat height = bannerHeigh;
     int th = 17;
     {
-        UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(height, 0, width - height, 17)];
-        title.text = dataObject.title;
-        title.font = [UIFont boldSystemFontOfSize:14];
-        title.textColor = [UIColor blackColor];
-        title.numberOfLines = 1;
-        [self.adView addSubview:title];
+        self.title.frame = CGRectMake(height, 0, width - height, 17);
+        self.title.text = dataObject.title;
+        self.title.font = [UIFont boldSystemFontOfSize:14];
+        self.title.textColor = [UIColor blackColor];
+        self.title.numberOfLines = 1;
     }
     {
-        UILabel *desc = [[UILabel alloc] initWithFrame:CGRectMake(height, th, width - height, height - th)];
-        desc.text = dataObject.desc;
-        desc.font = [UIFont systemFontOfSize:14];
-        desc.textColor = [UIColor blackColor];
-        desc.numberOfLines = 0;
-        [self.adView addSubview:desc];
+        self.desc.frame = CGRectMake(height, th, width - height, height - th);
+        self.desc.text = dataObject.desc;
+        self.desc.font = [UIFont systemFontOfSize:14];
+        self.desc.textColor = [UIColor blackColor];
+        self.desc.numberOfLines = 0;
     }
     self.adView.frame = CGRectMake(0, 0, width, bannerHeigh);
     self.adView.backgroundColor = [[UIColor grayColor] colorWithAlphaComponent:0.6];
     self.adView.logoView.frame = CGRectMake(CGRectGetWidth(self.adView.frame) - self.adView.logoImageViewDefaultWidth, CGRectGetHeight(self.adView.frame) - self.adView.logoImageViewDefaultHeight, self.adView.logoImageViewDefaultWidth, self.adView.logoImageViewDefaultHeight);
     [self.adView addSubview:self.adView.logoView];
-    [self.adView registerDataObjectWithClickableViews:@[iconImageView, self.adView]];
+    [self.adView registerDataObjectWithClickableViews:@[self.iconImageView, self.adView]];
 }
 
 + (CGFloat)cellHeightWithNativeAdDataObject:(XWNativeAdDataObject *)dataObject {
